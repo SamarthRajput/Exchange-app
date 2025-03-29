b7e021616775f83d45fd9fddb052f826540ffdbc
Samarth Rajput <ssamarth224@gmail.com>
2025-03-29 20:47:49 +0530

    detailing the frontend

--- exchange-frontend/app/component/MarketBar.tsx
+++ exchange-frontend/app/component/MarketBar.tsx
@@ -40,18 +40,18 @@ export const MarketBar = ({ market }: { market: string }) => {
                 <TickerRender market={market} />
                 <div className="flex items-center flex-row space-x-8 pl-4">
                     <div className="flex flex-col h-full justify-center">
-                        <p className={`font-medium tabular-nums text-greenText text-md text-green-500`}>${ticker?.lastPrice}</p>
-                        <p className="font-medium text-sm tabular-nums">${ticker?.lastPrice}</p>
+                        <p className={`font-medium tabular-nums text-greenText text-md text-green-500`}>${Number(ticker?.lastPrice).toLocaleString()}</p>
+                        <p className="font-medium text-sm tabular-nums">${Number(ticker?.lastPrice).toLocaleString()}</p>
                     </div>
                     <div className="flex flex-col">
                         <p className={`font-medium  text-slate-400 text-sm`}>24H Change</p>
                         <p className={` text-sm font-medium tabular-nums leading-5 text-greenText ${Number(ticker?.priceChange) > 0 ? "text-green-500" : "text-red-500"}`}>{Number(ticker?.priceChange) > 0 ? "+" : ""} {ticker?.priceChange} {Number(ticker?.priceChangePercent)?.toFixed(2)}%</p></div><div className="flex flex-col">
                             <p className="font-medium text-slate-400 text-sm">24H High</p>
-                            <p className="font-medium tabular-nums leading-5 text-sm ">{ticker?.high}</p>
+                            <p className="font-medium tabular-nums leading-5 text-sm ">{Number(ticker?.high).toLocaleString()}</p>
                             </div>
                             <div className="flex flex-col">
                                 <p className="font-medium text-slate-400 text-sm">24H Low</p>
-                                <p className=" font-medium tabular-nums leading-5 text-sm ">{ticker?.low}</p>
+                                <p className=" font-medium tabular-nums leading-5 text-sm ">{Number(ticker?.low).toLocaleString()}</p>
                             </div>
                         <button type="button" className="font-medium transition-opacity hover:opacity-80 hover:cursor-pointer text-base text-left" data-rac="">
                             <div className="flex flex-col">
