bc9866079c8de3d8c74283a421ed1b2585a8ce84
Samarth Rajput <ssamarth224@gmail.com>
2025-07-02 12:11:34 +0530

    changed url

--- exchange-frontend/app/utils/httpClient.ts
+++ exchange-frontend/app/utils/httpClient.ts
@@ -3,7 +3,7 @@ import { Depth, KLine, MarketData, Ticker, Trade } from "./types";
 
 // Basic backend points that we have to render them on our frontend 
 
-const BASE_URL = "https://exchange-proxy-server.onrender.com/";
+const BASE_URL = "http://localhost:3005/api/v1";
 
 // the getTicker function accepts market as an argument 
 export async function getTicker(market: string): Promise<Ticker> {
@@ -19,7 +19,7 @@ export async function getTicker(market: string): Promise<Ticker> {
 
 // the getTickers() function returns all the market eg SOL_USDC, ETH_USD etc
 export async function getTickers(): Promise<Ticker[]> {
-    const response = await axios.get(`${BASE_URL}api/v1/tickers`);
+    const response = await axios.get(`${BASE_URL}/tickers`);
     return response.data;
 }  
 
@@ -27,19 +27,19 @@ export async function getTickers(): Promise<Ticker[]> {
 // the api returns the type of the data, ki the asks and the bids are an array of tuples, tuple is an array with 2 values 
 // The getDepth function returns us the orderbook with asks and the bids 
 export async function getDepth(market: string): Promise<Depth> {
-    const response = await axios.get(`${BASE_URL}api/v1/depth?symbol=${market}`);
+    const response = await axios.get(`${BASE_URL}/depth?symbol=${market}`);
     return response.data;
 }
 
 // getTrades() function will return the recent trades that have happened on a specific market 
 export async function getTrades(market: string): Promise<Trade[]> {
-    const response = await axios.get(`${BASE_URL}api/v1/trades?symbol=${market}`);
+    const response = await axios.get(`${BASE_URL}/trades?symbol=${market}`);
     return response.data;
 }
 
 // The getKLines function represents all the chart data, basically a bunch of candles that we get back from the backend 
 export async function getKLines(market: string, interval: string, startTime: number, endTime: number): Promise<KLine[]> {
-    const response = await axios.get(`${BASE_URL}api/v1/klines?symbol=${market}&interval=${interval}&startTime=${startTime}&endTime=${endTime}`);
+    const response = await axios.get(`${BASE_URL}/klines?symbol=${market}&interval=${interval}&startTime=${startTime}&endTime=${endTime}`);
     const data: KLine[] = response.data;
     return data.sort((x, y) => (Number(x.end) < Number(y.end) ? -1 : 1));
 }
